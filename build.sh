731f0dde16fb50e02adc251c864f062d5ec4edfd
priyanshi chauhan <priyanshi.chauhan@opstree.com>
2024-03-19 15:04:33 +0530

    updated script

--- build.sh
+++ build.sh
@@ -11,6 +11,8 @@ git_url=$(getGitUrl)
 
 repo_name=$(basename "$git_url" | sed 's/\.git$//')
 
+repo_name=$(basename "$git_url" | sed 's/\.git$//')
+
 echo "BP_IMAGE_URI: $bp_image_uri"
 echo "BP_IMAGE_TAG: $bp_image_tag"
 echo "Repository Name: $repo_name"
@@ -19,13 +21,11 @@ getAssumeRole $IAM_ROLE_TO_ASSUME
 
 cd $WORKSPACE/$repo_name
 
-
 TASK_FAMILY=$(cat task-definition.json | jq -r '.family')
 
 TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition "$TASK_FAMILY" --region "$REGION")
 
 NEW_TASK_DEFINTIION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$bp_image_uri" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities) |  del(.registeredAt) | del(.registeredBy)')
-
 
 echo $NEW_TASK_DEFINTIION > tf.json
 
