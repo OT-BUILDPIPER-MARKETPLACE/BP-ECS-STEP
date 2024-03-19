b605d8b6c88a91a1228e8ea6786afaf04d2b5fc5
priyanshi chauhan <priyanshi.chauhan@opstree.com>
2024-03-19 15:02:11 +0530

    updated script

--- build.sh
+++ build.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
-source /app/buildpiper/shell-functions/functions.sh
-source /app/buildpiper/shell-functions/log-functions.sh
-source /app/buildpiper/shell-functions/str-functions.sh
-source /app/buildpiper/shell-functions/file-functions.sh
-source /app/buildpiper/shell-functions/aws-functions.sh
+source functions.sh
+source log-functions.sh
+source str-functions.sh
+source file-functions.sh
+source aws-functions.sh
 
 bp_image_uri=$(getComponentName)
 bp_image_tag=$(getRepositoryTag)
-
-# Extract ${BP_IMAGE_URI} and ${BP_IMAGE_TAG} using jq
-bp_image_uri=$(echo "$json_data" | jq -r '.addition_meta_data.placeholders[] | select(.key == "${BP_IMAGE_URI}") | .value')
-bp_image_tag=$(echo "$json_data" | jq -r '.addition_meta_data.placeholders[] | select(.key == "${BP_IMAGE_TAG}") | .value')
-git_url=$(echo "$json_data" | jq -r '.manifest_meta_data.manifest_git_repo.git_url')
+git_url=$(getGitUrl)
 
 repo_name=$(basename "$git_url" | sed 's/\.git$//')
 
-# Print the extracted values
 echo "BP_IMAGE_URI: $bp_image_uri"
 echo "BP_IMAGE_TAG: $bp_image_tag"
 echo "Repository Name: $repo_name"
